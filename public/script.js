f4b0490c6578c79a8280346943294f1446922d77
Shouraya <shourayagoyal2406@gmail.com>
2020-09-05 18:09:48 +0530

    two users added

--- public/script.js
+++ public/script.js
@@ -1,16 +1,47 @@
+const socket = io('/')
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true
+
+var peer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: '3000'
+}); 
 
 let myVideoStream
 // Get video and audio
 navigator.mediaDevices.getUserMedia({
     video: true, //true so that we need video, else false
     audio: true
-}).then(stream => {
+}).then(stream =>{
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+
+    peer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+          addVideoStream(video, userVideoStream)
+        })
+    })
+
+    socket.on('user-connected', (userId) => {
+        connecToNewUser(userId, stream);
+    })
 })
+
+peer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id);
+})
+
+const connecToNewUser = (userId, stream) => {
+   const call = peer.call(userId, stream)
+   const video = document.createElement('video')
+   call.on('stream', userVideoStream => {
+       addVideoStream(video, userVideoStream)
+   })
+}
 
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
