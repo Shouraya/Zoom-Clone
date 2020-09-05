ea9db9087b25abb2285f18da0b9c82f1ff605556
Shouraya <shourayagoyal2406@gmail.com>
2020-09-05 20:10:43 +0530

    Zoom Clone approach 1

--- app.js
+++ app.js
@@ -2,30 +2,39 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const { v4: uuidv4 } = require('uuid');
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
-    debug: true
+  debug: true
 });
-
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+const { v4: uuidV4 } = require('uuid')
 
 app.use('/peerjs', peerServer);
 
-app.get("/", (req,res) => {
-    res.redirect(`/${uuidv4()}`);
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
 })
 
-app.get("/:room", (req, res) => {
-    res.render('room', {roomId: req.params.room})
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
 })
 
 io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId)=> {
-        socket.join(roomId);
-        socket.to(roomId).broadcast.emit('user-connected', userId);
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    // messages
+    socket.on('message', (message) => {
+      //send message to the same room
+      io.to(roomId).emit('createMessage', message)
+  }); 
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
+  })
 })
 
-server.listen(3000);
+server.listen(process.env.PORT||3000);
